f4c76128d330d796662154ad02c61e243d0f7e78
ekakele <ekaakelenjeridze@gmail.com>
2024-01-22 10:33:14 +0400

    Initial Commit

---
+++ FinalProject_WineAppetit/Scenes/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FinalProject_WineAppetit
+//
+//  Created by Eka Kelenjeridze on 22.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
