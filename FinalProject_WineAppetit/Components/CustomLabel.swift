b17430a5d20bd6c5d1af0caf6e6b49f87e20ab10
ekakele <ekaakelenjeridze@gmail.com>
2024-01-23 22:46:43 +0400

    add resuable components: infoStack & customLabel

---
+++ FinalProject_WineAppetit/Components/CustomLabel.swift
@@ -0,0 +1,27 @@
+//
+//  CustomLabel.swift
+//  FinalProject_WineAppetit
+//
+//  Created by Eka Kelenjeridze on 23.01.24.
+//
+
+import UIKit
+
+class CustomLabel: UILabel {
+    //MARK: - Inits
+    init(
+        textColor: UIColor,
+        font: UIFont,
+        numberOfLines: Int
+    ) {
+        super.init(frame: .zero)
+        
+        self.textColor = textColor
+        self.font = font
+        self.numberOfLines = numberOfLines
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ FinalProject_WineAppetit/Components/ShortInfoStackView.swift
@@ -0,0 +1,31 @@
+//
+//  ShortInfoStackView.swift
+//  FinalProject_WineAppetit
+//
+//  Created by Eka Kelenjeridze on 23.01.24.
+//
+
+import UIKit
+
+class ShortInfoStackView: UIStackView {
+    //MARK: - Inits
+    init(
+        arrangedSubviews: [UIView],
+        axis: NSLayoutConstraint.Axis = .vertical,
+        distribution: UIStackView.Distribution = .equalSpacing,
+        stackAlignment: UIStackView.Alignment? = nil,
+        stackSpacing: CGFloat? = nil
+    ) {
+        super.init(frame: .zero)
+        
+        arrangedSubviews.forEach { self.addArrangedSubview($0) }
+        self.axis = axis
+        self.distribution = distribution
+        self.alignment = stackAlignment ?? .fill // Default alignment if nil is provided
+        self.spacing = stackSpacing ?? 0 // Default value
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
