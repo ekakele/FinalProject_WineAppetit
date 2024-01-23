8d7a3b9dc52398515110839d8555217a9205cfe2
ekakele <ekaakelenjeridze@gmail.com>
2024-01-23 19:33:06 +0400

    add AppConstants file to store constatnt values

---
+++ FinalProject_WineAppetit/Constants/AppConstants.swift
@@ -0,0 +1,16 @@
+//
+//  AppConstants.swift
+//  FinalProject_WineAppetit
+//
+//  Created by Eka Kelenjeridze on 23.01.24.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct API {
+        static let wineApiBaseURL = "https://api.openwine.space/"
+        static let wineApiEndpoint = "api/wines?page=1"
+    }
+}
