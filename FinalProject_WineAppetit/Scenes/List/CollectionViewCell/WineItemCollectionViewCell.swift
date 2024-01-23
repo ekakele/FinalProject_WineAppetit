2c8d6991fc6c70b447ac27f9540c137d7f2681b9
ekakele <ekaakelenjeridze@gmail.com>
2024-01-23 14:21:18 +0400

    add NetworkManager Package Dependency

---
+++ FinalProject_WineAppetit/Scenes/List/CollectionViewCell/WineItemCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  WineItemCollectionViewCell.swift
+//  FinalProject_WineAppetit
+//
+//  Created by Eka Kelenjeridze on 23.01.24.
+//
+
+import UIKit
+
+class WineItemCollectionViewCell: UICollectionViewCell {
+    
+}
