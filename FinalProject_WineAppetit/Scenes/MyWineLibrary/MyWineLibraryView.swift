b82a64cb136c2a52f1d187077b484820c9883314
ekakele <ekaakelenjeridze@gmail.com>
2024-01-31 20:33:44 +0400

    setup reusable component: ShelfTitleView

---
+++ FinalProject_WineAppetit/Scenes/MyWineLibrary/MyWineLibraryView.swift
@@ -0,0 +1,18 @@
+//
+//  MyWineLibraryView.swift
+//  FinalProject_WineAppetit
+//
+//  Created by Eka Kelenjeridze on 31.01.24.
+//
+
+import SwiftUI
+
+struct MyWineLibraryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyWineLibraryView()
+}
---
+++ FinalProject_WineAppetit/Scenes/MyWineLibrary/MyWineLibraryViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MyWineLibraryViewModel.swift
+//  FinalProject_WineAppetit
+//
+//  Created by Eka Kelenjeridze on 31.01.24.
+//
+
+import Foundation
