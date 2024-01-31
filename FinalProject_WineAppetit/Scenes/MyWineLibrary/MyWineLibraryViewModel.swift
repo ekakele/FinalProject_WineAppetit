92370ff96602198a482a70578ac34aef92c7f891
ekakele <ekaakelenjeridze@gmail.com>
2024-01-31 21:05:17 +0400

    setup MyWineLibraryViewModel

--- FinalProject_WineAppetit/Scenes/MyWineLibrary/MyWineLibraryViewModel.swift
+++ FinalProject_WineAppetit/Scenes/MyWineLibrary/MyWineLibraryViewModel.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+import GenericNetworkManager
+
+final class MyWineLibraryViewModel: ObservableObject {
+    // MARK: - Init
+    @Published var wines: [Wine] = []
+    private let baseURL = Constants.API.wineApiBaseURL
+    
+    // MARK: - Init
+    init() {
+        fetchWines()
+    }
+    
+    // MARK: - Methods
+    func filteredWines(category: String) -> [Wine] {
+        let category = category.uppercased()
+        let filteredWines = wines.filter { $0.categoriesList[0] == category }
+        return filteredWines
+    }
+    
+    // MARK: - Private Methods
+    private func fetchWines() {
+        let endpoint = "api/wines?page=1"
+        var favoriteWineIDList = UserPreferencesManager.shared.getFavoriteWineList().map { String($0) }.joined(separator: ",")
+        var queryString = "&ids=" + favoriteWineIDList
+        
+        let urlString = baseURL + endpoint + queryString
+        print(urlString)
+        
+        GenericNetworkManager.shared.fetchData(with: urlString) { [weak self] (result: Result<WineData, Error>) in
+            switch result {
+            case .success(let wines):
+                self?.wines = wines.result
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
