08c6b9c8e91bdb2842b54ac79a22b080d54f7577
ekakele <ekaakelenjeridze@gmail.com>
2024-02-01 13:24:58 +0400

    add WineListViewController's representable view to use in MyWineLibraryView

---
+++ FinalProject_WineAppetit/Components/SwiftUIComponents/WineListViewControllerRepresentableView.swift
@@ -0,0 +1,30 @@
+//
+//  WineListViewControllerRepresentableView.swift
+//  FinalProject_WineAppetit
+//
+//  Created by Eka Kelenjeridze on 01.02.24.
+//
+
+import SwiftUI
+
+struct WineListViewControllerRepresentableView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        let wineListViewController = WineListViewController()
+        return UINavigationController(rootViewController: wineListViewController)
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+}
+
+// MARK: - Coordinator
+class Coordinator: NSObject {
+}
+
+#Preview {
+    WineListViewControllerRepresentableView()
+}
