d2b0377dc682e723cc7daef813884c4f9963119d
ekakele <ekaakelenjeridze@gmail.com>
2024-02-03 01:52:21 +0400

    add spinning animation to the bottle image

--- FinalProject_WineAppetit/Scenes/SpinTheBottle/SpinTheBottleViewController.swift
+++ FinalProject_WineAppetit/Scenes/SpinTheBottle/SpinTheBottleViewController.swift
@@ -32,12 +32,59 @@ class SpinTheBottleViewController: UIViewController {
         return imageView
     }()
     
+    private var lastRotation: CGFloat = 0.0
+    private var angularVelocity: CGFloat = 0.0
     
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupBottlePanGestureRecognizer()
         setupUI()
+    }
+    
+    //MARK: - Private Methods
+    private func setupBottlePanGestureRecognizer() {
+        bottleImage.isUserInteractionEnabled = true
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handleBottlePanGesture(_:)))
+        bottleImage.addGestureRecognizer(panGesture)
+    }
+    
+    @objc private func handleBottlePanGesture(_ gesture: UIPanGestureRecognizer) {
+        let touchPoint = gesture.location(in: view)
+        let centerPoint = bottleImage.center
+        
+        let currentAngle = atan2(touchPoint.y - centerPoint.y, touchPoint.x - centerPoint.x)
+        
+        switch gesture.state {
+        case .began:
+            lastRotation = currentAngle
+        case .changed:
+            let angleDifference = currentAngle - lastRotation
+            angularVelocity = angleDifference
+            bottleImage.transform = bottleImage.transform.rotated(by: angleDifference)
+            lastRotation = currentAngle
+        case .ended, .cancelled:
+            decelerateBottleRotation()
+        default:
+            break
+        }
+    }
+    
+    private func decelerateBottleRotation() {
+        let decelerationRate = CGFloat(0.95)
+        let minimumVelocity = CGFloat(0.01)
+        
+        Timer.scheduledTimer(withTimeInterval: 0.02, repeats: true) { [weak self] timer in
+            guard let self = self else { return }
+            
+            self.angularVelocity *= decelerationRate
+            self.bottleImage.transform = self.bottleImage.transform.rotated(by: self.angularVelocity)
+            
+            if abs(self.angularVelocity) < minimumVelocity {
+                timer.invalidate()
+            }
+        }
     }
     
     //MARK: - UI Setup
@@ -65,7 +112,7 @@ class SpinTheBottleViewController: UIViewController {
             backgroundImage.topAnchor.constraint(equalTo: view.topAnchor),
             backgroundImage.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             backgroundImage.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            backgroundImage.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            backgroundImage.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
 }
