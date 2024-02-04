d5eaf18ad2c9da9c525081a1fe7370f840a62beb
ekakele <ekaakelenjeridze@gmail.com>
2024-02-04 05:05:55 +0400

    update whiteWine's value in AppUIColor

--- FinalProject_WineAppetit/Constants/AppConstants.swift
+++ FinalProject_WineAppetit/Constants/AppConstants.swift
@@ -56,7 +56,7 @@ struct Constants {
         static let grayStroke = UIColor(hex: "#7f7f7f")
         
         static let redWine = UIColor(hex: "#800020").cgColor
-        static let whiteWine = UIColor(hex: "#FFBF00").cgColor
+        static let whiteWine = UIColor(hex: "#5C7A13").cgColor
         static let roseWine = UIColor(hex: "#9B0047").cgColor
         static let white = UIColor(hex: "#F8EDEB").cgColor
         
@@ -68,6 +68,7 @@ struct Constants {
         static let darkGreen = UIColor(hex: "#132a13")
         static let burgundy = UIColor(hex: "#540804")
     }
+//    5c7a13
     
     //MARK: - SwiftUI
     struct AppColor {
