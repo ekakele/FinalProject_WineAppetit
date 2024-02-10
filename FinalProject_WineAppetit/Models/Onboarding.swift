a21e0bdcd959d659c96762c0ce82e74d983565ac
ekakele <ekaakelenjeridze@gmail.com>
2024-02-10 16:09:21 +0400

    add Onboarding Model

---
+++ FinalProject_WineAppetit/Models/Onboarding.swift
@@ -0,0 +1,15 @@
+//
+//  Onboarding.swift
+//  FinalProject_WineAppetit
+//
+//  Created by Eka Kelenjeridze on 10.02.24.
+//
+
+import Foundation
+
+struct Onboarding: Identifiable {
+    let id = UUID()
+    let imageName: String
+    let title: String
+    let description: String
+}
