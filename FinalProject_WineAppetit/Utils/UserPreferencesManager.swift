e0b03122ed5f619c486206d2f0201310cd23e66c
ekakele <ekaakelenjeridze@gmail.com>
2024-02-28 23:57:24 +0400

    reset onboardingPassStatusKey

--- FinalProject_WineAppetit/Utils/UserPreferencesManager.swift
+++ FinalProject_WineAppetit/Utils/UserPreferencesManager.swift
@@ -13,7 +13,7 @@ final class UserPreferencesManager {
     private let defaults = UserDefaults.standard
     
     private let favoritedWineListKey = "favoriteWines"
-    private let onboardingPassStatusKey = "isOnboardingPassed98"
+    private let onboardingPassStatusKey = "isOnboardingPassed"
     
     // MARK: - OnboardingPassStatus Set/Get Methods
     func getOnboardingPassStatus() -> Bool {
